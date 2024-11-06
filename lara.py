8ec0b083c5eacc20c9718eaf55a267ff79326894
backendGuru88 <adesewalola2006@gmail.com>
2024-11-06 13:55:14 +0100

    second commit

--- lara.py
+++ lara.py
@@ -1,5 +1,15 @@
 def calculate_discount(price,discount_percent):
     discount_amount = price * discount_percent
-    return discount_amount
+   
+
+    if discount_percent  >= 20:
+        # print("You get a discount of ",discount_amount)
+        return discount_amount
+    else:
+        return price
+
+        
+
+
 
 print(calculate_discount(int(input("Enter the price: ")),float(input("Enter the discount percent: "))))
